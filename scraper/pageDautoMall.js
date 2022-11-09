d2dc23a7065269a3d1b8eb67ebc33567946ddde9
thangld <thangld2407@gmail.com>
2022-11-09 23:42:05 +0700

    loop page pagination

--- scraper/pageDautoMall.js
+++ scraper/pageDautoMall.js
@@ -8,13 +8,20 @@ const scraperObject = {
       let dataFile = [];
       let pageTemp = await browser.newPage();
 
-      console.log(`Navigating to ${this.url}/BuyCar/BuyCarDomesticBody.do ...`);
+      await pageTemp.setViewport({ width: 1500, height: 800 });
 
-      await pageTemp.goto(`${this.url}/BuyCar/BuyCarDomesticBody.do`);
+      console.log(`Navigating to ${this.url}/BuyCar/BuyCarDomesticBody.do ...`);
+      await pageTemp.goto(`${this.url}/BuyCar/BuyCarDomesticList.do`);
+
+      const frame = pageTemp
+        .frames()
+        .find((frame) => frame.name() === "body_IFrame");
+
+      await frame.waitForSelector(".form1");
 
       await pageTemp.waitForSelector(".secMdle");
 
-      let totalPage = await pageTemp.evaluate(() => {
+      let totalPage = await frame.evaluate(() => {
         let lastPage = document
           .querySelector(".pageWp .pagination a.NextNext")
           .getAttribute("onclick")
@@ -25,41 +32,61 @@ const scraperObject = {
         return lastPage;
       });
 
-      console.log("Closing the pageTemp...");
-      await pageTemp.close();
+      console.log("Đã lấy được tổng số trang là", totalPage);
 
-      // for (let i = 1; i <= totalPage; i++) {
-      let page = await browser.newPage();
-      console.log(`Navigating to ${this.url}/BuyCar/BuyCarDomesticList.do ...`);
-      await page.goto(`${this.url}/BuyCar/BuyCarDomesticList.do`);
-      const frame = page
-        .frames()
-        .find((frame) => frame.name() === "body_IFrame");
+      let page = 1;
+      do {
+        page++;
+        console.log(page);
+        // Lấy số pagination trong 1 trang
+        let pagination = await frame.evaluate(() => {
+          let pagination = document.querySelectorAll(".pageWp .pagination a");
+          return pagination.length;
+        });
 
-      await frame.waitForSelector(".form1");
+        console.log("Đã lấy được số pagination là", pagination);
+        for (let i = 2; i <= 4; i++) {
+          console.log("Chuyển đến trang số", i - 1);
+          await frame.click(`.pageWp .pagination a:nth-child(${i + 1})`, {
+            waitUntil: "networkidle2",
+          });
+          await frame.waitForSelector(".secMdle .tb01");
 
-      let data = await frame.evaluate(() => {
-        let data = [];
-        let elements = document.querySelectorAll(".tb01 tbody tr");
+          let dt = await frame.evaluate(() => {
+            let data = [];
+            let elements = document.querySelectorAll(".tb01 tbody tr");
 
-        for (let el of elements) {
-          // let obj = {};
-          let photo = el && el.querySelector(".TDmodel .photo");
-          if (photo) {
-            data.push(photo.querySelector("img").src);
-          }
+            for (let el of elements) {
+              let obj = {};
+              let photo = el && el.querySelector(".TDmodel .photo img");
+
+              if (el && el.getAttribute("onclick")) {
+                let link = el.getAttribute("onclick");
+                obj.link = link;
+              }
+
+              if (photo) {
+                obj.image = photo.src;
+              }
+
+              if (Object.keys(obj).length > 0) {
+                data.push(obj);
+              }
+            }
+            return data;
+          });
+
+          dataFile = [...dataFile, ...dt];
         }
+        await frame.click(" .pageWp .pagination a.arrowNext", { waitUntil: "networkidle2" });
+        await frame.waitForSelector(".secMdle .tb01");
+      } while (page < totalPage / 10);
 
-        return data;
-      });
-
-      dataFile.push(data);
-      console.log(dataFile);
-      // }
-
-      await browser.close();
+      console.log("Đã lấy được tổng số dòng là", dataFile[dataFile.length - 1]);
+      fs.writeFileSync("dautomall.json", JSON.stringify(dataFile));
+      await pageTemp.close();
     } catch (error) {
-      // this.scraper(browser);
+      this.scraper(browser);
       console.log(error);
     }
   },
