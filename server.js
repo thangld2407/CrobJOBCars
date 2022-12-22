3f5ea5880c4f2b6ca694932949e24549dcf15599
thangld <thangld2407@gmail.com>
2022-12-22 23:34:13 +0700

    add cron job

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+require("dotenv").config();
+const cron = require("node-cron");
 const PageDjautoController = require("./controller/PageDjautoController");
 
-PageDjautoController();
+cron.schedule("0 */8 * * *", async () => {
+  CrawlData();
+});
+
+function CrawlData() {
+  PageDjautoController();
+}
