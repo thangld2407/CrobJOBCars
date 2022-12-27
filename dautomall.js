1afec2acb6e061e946a041d04523d06e42229937
thangld <thangld2407@gmail.com>
2022-12-28 00:02:56 +0700

    add cronjob

--- dautomall.js
+++ dautomall.js
@@ -3,3 +3,7 @@ const cron = require("node-cron");
 const scrapDautomall = require("./scraper/page_dautomall");
 
 scrapDautomall();
+
+cron.schedule("0 */10 * * *", async () => {
+  scrapDautomall();
+});
