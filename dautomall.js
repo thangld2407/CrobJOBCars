a36a71aab9317550abed517726cb47f11a2f6525
Thang Le Dinh <thangld2407@gmail.com>
2023-01-04 10:52:55 +0700

    remove fn

--- dautomall.js
+++ dautomall.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 const cron = require("node-cron");
 const scrapDautomall = require("./scraper/page_dautomall");
 
-scrapDautomall();
-
 cron.schedule("0 4 * * *", async () => {
   scrapDautomall();
 });
